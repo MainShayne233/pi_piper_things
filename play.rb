14e2ce200fb2387fc908f535abaa56b99c089192
MainShayne233 <shaynetremblay@hotmail.com>
2016-02-16 22:42:12 -0500

    car_tune

--- play.rb
+++ play.rb
@@ -4,20 +4,18 @@ pin1 = PiPiper::Pin.new(pin: 17, direction: :out)
 pin2 = PiPiper::Pin.new(pin: 18, direction: :out)
 pin3 = PiPiper::Pin.new(pin: 27, direction: :out)
 pin4 = PiPiper::Pin.new(pin: 22, direction: :out)
-pin1.off
-pin2.off
-pin3.off
-pin4.off
 
-#pin1.on
-#pin2.on
-#pin3.on
-pin4.on
-sleep 1
-pin1.off
-pin2.off
-pin3.off
-pin4.off
+off_all()
+forward()
+off_all()
+
+
+def off_all()
+  pin1.off
+  pin2.off
+  pin3.off
+  pin4.off
+end
 
 def right_forward()
   pin1.on
@@ -29,4 +27,14 @@ end
 
 def left_forward()
   pin3.on
+end
+
+def left_backward()
+  pin4.on
+end
+
+def forward()
+  pin1.on
+  pin3.on
+  sleep 1
 end
